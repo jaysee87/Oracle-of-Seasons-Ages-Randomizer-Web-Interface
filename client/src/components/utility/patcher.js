2f7c35c2b1ee00bbf452e5ed3dcc7c16c9f4ab59
Matthew Stewart <stewartmatthew6@gmail.com>
2019-07-03 17:45:43 -0400

    Move some patching code out of a loop

--- client/src/components/utility/patcher.js
+++ client/src/components/utility/patcher.js
@@ -31,36 +31,36 @@ function patchPaletteData(rom_array, paletteIndex, ages){
       }
       addr += 4;
     }
+  }
 
-    // Address of standard sprite palettes
-    const paletteSrc = ages ? 0x5c8f0 : 0x58840;
-    // Free space to put modified copy of sprite palettes for file select screen
-    const fileSelectPaletteAddr = ages ? 0x5eee3 : 0x5ba07;
-    // Copy normal palettes
-    for (let x=8; x<8*4; x++)
-      rom_array[fileSelectPaletteAddr + x] = rom_array[paletteSrc + x];
-    // Replace first palette (green) with appropriate one
-    for (let x=0; x<8; x++)
-      rom_array[fileSelectPaletteAddr + x] = rom_array[paletteSrc + x + paletteIndex * 8];
-    // Replace file select's palette reference to newly created data
-    if (ages) {
-      writePointer(rom_array, 0x64de, fileSelectPaletteAddr);
-      writePointer(rom_array, 0x64ea, fileSelectPaletteAddr);
-    }
-    else {
-      writePointer(rom_array, 0x6428, fileSelectPaletteAddr);
-      writePointer(rom_array, 0x6434, fileSelectPaletteAddr);
-    }
+  // Address of standard sprite palettes
+  const paletteSrc = ages ? 0x5c8f0 : 0x58840;
+  // Free space to put modified copy of sprite palettes for file select screen
+  const fileSelectPaletteAddr = ages ? 0x5eee3 : 0x5ba07;
+  // Copy normal palettes
+  for (let x=8; x<8*4; x++)
+    rom_array[fileSelectPaletteAddr + x] = rom_array[paletteSrc + x];
+  // Replace first palette (green) with appropriate one
+  for (let x=0; x<8; x++)
+    rom_array[fileSelectPaletteAddr + x] = rom_array[paletteSrc + x + paletteIndex * 8];
+  // Replace file select's palette reference to newly created data
+  if (ages) {
+    writePointer(rom_array, 0x64de, fileSelectPaletteAddr);
+    writePointer(rom_array, 0x64ea, fileSelectPaletteAddr);
+  }
+  else {
+    writePointer(rom_array, 0x6428, fileSelectPaletteAddr);
+    writePointer(rom_array, 0x6434, fileSelectPaletteAddr);
+  }
 
-    if (paletteIndex >= 4) {
-      // For "inverted" palettes, change the damage-taking palette to 1 or
-      // 2 (depending if it's red or blue).
-      const damagePalette = (paletteIndex-3) | 8;
-      if (ages)
-        rom_array[0x14290] = damagePalette;
-      else
-        rom_array[0x1424e] = damagePalette;
-    }
+  if (paletteIndex >= 4) {
+    // For "inverted" palettes, change the damage-taking palette to 1 or
+    // 2 (depending if it's red or blue).
+    const damagePalette = (paletteIndex-3) | 8;
+    if (ages)
+      rom_array[0x14290] = damagePalette;
+    else
+      rom_array[0x1424e] = damagePalette;
   }
 }
 
